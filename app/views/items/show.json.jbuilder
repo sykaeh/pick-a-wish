7e6881124d4447d96a0bd0a48270f72daff019d4
Sybil Ehrensberger <sybil.ehrensberger@gmail.com>
2015-12-12 22:50:44 +0100

    Initial commit

---
+++ app/views/items/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @item, :id, :name, :description, :list_id, :claimed_on, :claimed_by_id, :created_at, :updated_at
---
+++ test/models/item_test.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: items
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  list_id     :integer
+#  claimed_on  :datetime
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require 'test_helper'
+
+class ItemTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
